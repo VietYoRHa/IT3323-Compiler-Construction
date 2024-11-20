02c800aacb8bbfb17b18bd34347fbac9f6cd6888
VietYoRHa <thaiviet04@gmail.com>
2024-11-20 10:33:19 +0700

    update main function to use argv

--- scanner.c
+++ scanner.c
@@ -556,29 +556,35 @@ int scan(char *fileName)
 
 /******************************************************************/
 
-/*int main(int argc, char *argv[]) {
-  if (argc <= 1) {
-    printf("scanner: no input file.\n");
-    return -1;
-  }
-
-  if (scan(argv[1]) == IO_ERROR) {
-    printf("Can\'t read input file!\n");
-    return -1;
-  }
-
-  return 0;
-}*/
-
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc <= 1)
+  {
+    if (scan(TEST_FILE) == IO_ERROR)
+    {
+      printf("Can\'t read input file!\n");
+      return -1;
+    }
+  }
 
-  if (scan(TEST_FILE) == IO_ERROR)
+  else if (scan(argv[1]) == IO_ERROR)
   {
     printf("Can\'t read input file!\n");
+    return -1;
   }
+
   return 0;
 }
+
+// int main()
+// {
+
+//   if (scan(TEST_FILE) == IO_ERROR)
+//   {
+//     printf("Can\'t read input file!\n");
+//   }
+//   return 0;
+// }
 
 /***************************************************************/
 // Function Definition
